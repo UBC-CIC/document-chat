b77918e2f6780628b3d9e0411d916188c1ee6298
Arshia Moghimi <arshiamoghimi98@gmail.com>
2024-07-17 16:13:02 -0700

    feat: added vpc stack template

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { BackendStack } from '../lib/backend-stack';
 import { FrontendStack } from '../lib/frontend-stack';
+import { VpcStack } from "../lib/vpc-stack";
 
 const app = new cdk.App();
 
@@ -11,5 +12,6 @@ const env = {
     region: process.env.CDK_DEFAULT_REGION 
   };
 
-const backendStack = new BackendStack(app, 'BackendStack', { env });
+const vpcStack = new VpcStack(app, 'VpcStack', {env})
+const backendStack = new BackendStack(app, 'BackendStack', vpcStack, { env });
 const frondendStack = new FrontendStack(app, 'FrontendStack', { env });
--- cdk/lib/vpc-stack.ts
+++ cdk/lib/vpc-stack.ts
@@ -2,10 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-import { VpcStack } from './vpc-stack';
-
-export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, vpcStack: VpcStack, props?: cdk.StackProps) {
+export class VpcStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
     // example resource
