8104a549672159d82f5f001902d1b2ff32c9000d
Arshia Moghimi <arshiamoghimi98@gmail.com>
2024-07-24 12:02:42 -0700

    feat: added vpc to cdk (#2)

--- cdk/lib/vpc-stack.ts
+++ cdk/lib/vpc-stack.ts
@@ -1,14 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class VpcStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const natGatewayProvider = ec2.NatProvider.gateway()
+
+    this.vpc = new ec2.Vpc(this, "lci", {
+      vpcName: "lci",
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      natGatewayProvider: natGatewayProvider,
+      natGateways: 1,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "private",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ]
+    })
+
   }
 }
