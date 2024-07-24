e5a021bf70534f062c887e9aee1f4c6ec7dcbe19
Arshia Moghimi <arshiamoghimi98@gmail.com>
2024-07-24 16:10:42 -0700

    feat: added backend stack

---
+++ cdk/lambda/uploader.py
@@ -0,0 +1,14 @@
+import os
+import boto3
+
+s3_client = boto3.client('s3')
+
+def handler(event, context):
+    object_key = event['object_key']
+    bucket_name = os.getenv('DOCUMENTS_BUCKET')
+    response = s3_client.generate_presigned_post(bucket_name, object_key)
+    response = {
+        'statusCode': 200,
+        'body': response
+    }
+    return response
