772204058a26243dd80dafb391dee4b31b7823f6
David Mwita <david.sam0051@gmail.com>
2024-07-31 17:44:21 -0700

    api connected: uploading and prompting working

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,20 +1,20 @@
 import { Box, IconButton, List, ListItem, Typography } from '@mui/material';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import { useState, useRef } from 'react';
+import DocumentCard from './DocumentCard';
+import { bg_color_3 } from './consts';
 
 const Navigation = () => {
   const [uploadedDocs, setUploadedDocs] = useState([]);
   const fileInputRef = useRef(null);
 
-  const bg_color = "#eeeeee";
-
   const handleFilePickerClick = () => {
     fileInputRef.current.click();
   };
 
-  const handleFileChange = async (event) => {
+  const handleFileUpload = async (event) => {
     const files = Array.from(event.target.files);
-    const pdfFiles = files.filter(file => file.type === 'application/pdf');
+    const pdfFiles = files.filter(file => file.type === 'application/pdf'); // Filter only PDF files
 
     /** TODO: Handle multiple file uploads & lock screen while uploading files. */
     pdfFiles.forEach(async file => {
@@ -24,24 +24,39 @@ const Navigation = () => {
         });
 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok. Failed to get signed URL.');
         }
 
-        const data = await response.json();
-        console.log(`Signed URL for ${file.name}: ${data.url}`);
+        // Read the response as a stream
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder('utf-8');
+        let result = '';
+        let done = false;
 
-        /** upload is commented until CORS fixed 
-      
-        // Upload the document to the signed URL // TODO: Verify
-        const uploadResponse = await fetch(data.url, {
-          method: 'POST',
-          body: {
-            data: data.fields,
-            files: file,
-          },
-          headers: {
-            'Content-Type': 'application/pdf',
-          },
+        while (!done) {
+          const { value, done: doneReading } = await reader.read();
+          done = doneReading;
+          result += decoder.decode(value, { stream: !done });
+        }
+
+        // Decode base64 to JSON object
+        const presignedResponse = JSON.parse(atob(result));
+        console.log(`Signed URL for ${file.name}: ${presignedResponse.url}`);
+
+        // Upload the document to the signed URL
+        const formData = new FormData();
+        Object.entries({...presignedResponse.fields}).forEach(([key, value]) => {
+          formData.append(key, value)
+        });
+        formData.append('file', file);
+
+        formData.forEach((value, key) => {
+          console.log(`${key}: ${value}`);
+        })
+
+        const uploadResponse = await fetch(presignedResponse.url, {
+          method: "POST",
+          body: formData
         });
 
         if (!uploadResponse.ok) {
@@ -49,40 +64,43 @@ const Navigation = () => {
         }
 
         console.log(`Successfully uploaded ${file.name}`);
-
         setUploadedDocs(prevDocs => [...prevDocs, file.name]);
 
-        */
       } catch (error) {
-        console.error(`Failed to get signed URL for ${file.name}:`, error);
+        console.error(`Failed to upload ${file.name}:`, error);
       }
 
       
     });
   };
 
+  const deleteFile = (index) => {
+    // TODO: Implement delete file functionality on API
+    setUploadedDocs(prevDocs => prevDocs.filter((_, i) => i !== index));
+  }
+
   return (
-    <Box width="10%" bgcolor={bg_color} display="flex" flexDirection="column" p={2}>
+    <Box width="20%" bgcolor={bg_color_3} display="flex" flexDirection="column" p={2}>
 
       <Box pt={6}>
         <Typography variant="h7">Documents</Typography>
         <List>
           {uploadedDocs.map((filename, index) => (
-            <ListItem key={index} style={{ paddingLeft: '20px' }}>
-              <Typography variant='body2'>{filename} </Typography>
+            <ListItem key={index}>
+              <DocumentCard documentName={filename} index={index} deleteFunction={deleteFile} />
             </ListItem>
           ))}
         </List>
       </Box>
 
-      <Box position="absolute" bottom={'10px'} left={'3.5%'} p={2} bgcolor={bg_color} textAlign="center">
+      <Box position="absolute" bottom={'10px'} left={'3.5%'} p={2} bgcolor={bg_color_3} textAlign="center">
         <input
           accept="application/pdf"
           style={{ display: 'none' }}
           id="upload-button-file"
           type="file"
           ref={fileInputRef}
-          onChange={handleFileChange}
+          onChange={handleFileUpload}
         />
         <IconButton onClick={handleFilePickerClick}>
           <AttachFileIcon />
