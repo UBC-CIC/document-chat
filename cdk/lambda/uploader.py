931af3634cecb2837a43ab57a08958d7dd3dabf6
Arshia Moghimi <arshiamoghimi98@gmail.com>
2024-07-31 17:46:21 -0700

    CORS Fix (#5)
    * feat: added vpc to cdk
    
    * feat: added backend stack
    
    * feat: added api gateway
    
    * fix: api gateway parameters in lambda
    
    * fix: api gateway
    
    * fix: cors error

--- cdk/lambda/uploader.py
+++ cdk/lambda/uploader.py
@@ -11,6 +11,11 @@ def handler(event, context):
     response = s3_client.generate_presigned_post(bucket_name, object_key)
     response = {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*', # TODO: fix for production
+            'Access-Control-Allow-Methods': 'GET'
+        },
         'body': base64.b64encode(json.dumps(response, indent=2).encode('utf-8'))
     }
     return response
