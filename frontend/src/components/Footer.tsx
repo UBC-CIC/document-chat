6ebef19524435966d77f61ab5a8b7086c3c09263
David Mwita <dmwita@student.ubc.ca>
2024-09-03 18:57:48 -0700

    update footer link to github repo

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer: React.FC = () => {
               width={20}
               className="mr-1.5 py-2 mx-2"
             />
-            <a href="https://github.com/UBC-CIC/LCI-forestry">
+            <a href="https://github.com/UBC-CIC/document-chat">
               Source code on GitHub
             </a>
           </div>
